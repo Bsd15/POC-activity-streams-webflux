11ce12358b2b59db81ea7819f4036bc7d91ef298
sivadatta.bathina <sivadatta.bathina@cgi.com>
2019-08-26 10:17:08 +0530

    Added RequestMapping to return Flux<Activity>.

--- README.md
+++ README.md
@@ -6,5 +6,14 @@ Dependencies:
 
 - [Clone and install OpenSocial/activitystreams as Maven Dependency](https://github.com/OpenSocial/activitystreams)
 
+Add the below dependency after installing the above into Maven
+```xml
+<dependency>
+    <groupId>com.ibm.common</groupId>
+	<artifactId>activitystreams-ext</artifactId>
+	<version>0.0.1-SNAPSHOT</version>
+</dependency>
+```
+
 References:
 - [Activity streams with Spring Boot](https://github.com/Bsd15/POC-Activity-Streams)
--- src/main/java/com/stackroute/succour/webfluxactivitystreams/controller/WebFluxController.java
+++ src/main/java/com/stackroute/succour/webfluxactivitystreams/controller/WebFluxController.java
@@ -1,5 +1,6 @@
 package com.stackroute.succour.webfluxactivitystreams.controller;
 
+import com.ibm.common.activitystreams.Activity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +9,9 @@ import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 
 import java.util.Collections;
+
+import static com.ibm.common.activitystreams.Makers.activity;
+import static com.ibm.common.activitystreams.Makers.object;
 
 @RestController
 public class WebFluxController {
@@ -20,8 +24,13 @@ public class WebFluxController {
             .build();
 
     @RequestMapping("/webflux-activity-streams")
-    public Flux getActivityStreams(){
-
-        return null;
+    public Flux<Activity> getActivityStreams(){
+        Activity activity =
+                activity()
+                        .actor(object("person").id("acct:joe@example.org"))
+                        .object(object("note").content("my note"))
+                        .verb("post")
+                        .get();
+        return Flux.just(activity);
     }
 }
